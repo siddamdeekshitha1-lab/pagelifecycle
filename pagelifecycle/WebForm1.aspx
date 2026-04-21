52d5e970f49d3407082934101f543d010338caf4
ADMIN <ADMIN@DESKTOP-VBQVFV9>
2026-04-21 12:50:40 +0530

    Add project files.

---
+++ pagelifecycle/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="pagelifecycle.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+    <asp:Label ID="lblmsg" runat="server"></asp:Label>
+    <asp:Label ID="label1" runat="server"></asp:Label>
+    <asp:Label ID="label2" runat="server"></asp:Label>
+    <asp:Label ID="label3" runat="server"></asp:Label>
+    <asp:Label ID="label4" runat="server"></asp:Label>
+    <br />
+    <asp:TextBox ID="txtusername" runat="server" OnTextChanged="txtusername_TextChanged"></asp:TextBox><br />
+    <asp:Button ID="btnsubmit" runat="server" Text="SUBMIT" OnClick="btnsubmit_Click" /><br />
+    <asp:RadioButton ID="rbtnTelugu" runat="server" GroupName="language" AutoPostBack="true" OnCheckedChanged="rbtnTelugu_CheckedChanged" />Telugu
+    <asp:Label ID="lbltelugu" runat="server"></asp:Label><br />
+    <asp:RadioButton ID="rbtnhindi" runat="server" GroupName="language" AutoPostBack="true" OnCheckedChanged="rbtnhindi_CheckedChanged" />Hindi
+    <asp:Label ID="lblhindi" runat="server"></asp:Label><br />
+    <asp:RadioButton ID="rbtnEnglish" runat="server" GroupName="language" AutoPostBack="true" OnCheckedChanged="rbtnEnglish_CheckedChanged" />English
+    <asp:Label ID="lblenglish" runat="server"></asp:Label>
+    <br />
+    <asp:CheckBox ID="chkchess" runat="server" AutoPostBack="true" OnCheckedChanged="chkchess_CheckedChanged" />Chess
+                <asp:Label ID="lblmesg" runat="server"></asp:Label><br />
+    <asp:CheckBox ID="chkTennis" runat="server" AutoPostBack="true" OnCheckedChanged="chkTennis_CheckedChanged" />Tennis
+                <asp:Label ID="lbltennis" runat="server"></asp:Label><br />
+    <asp:CheckBox ID="chkcricket" runat="server" AutoPostBack="true" OnCheckedChanged="chkcricket_CheckedChanged" />Cricket
+                <asp:Label ID="lblcricket" runat="server"></asp:Label><br />
+
+</div>
+    </form>
+</body>
+</html>
---
+++ pagelifecycle/WebForm1.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pagelifecycle
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            label3.Text = "Hi page load event<br>";
+            btnsubmit.Visible = false;
+        }
+        protected void page_preINIT(object sender, EventArgs e)
+        {
+            lblmsg.Text = "Hi page pre init event<br>";
+        }
+        protected void page_INIT(object sender, EventArgs e)
+        {
+            label1.Text = "hii page_INIT event<br>";
+        }
+        protected void page_INITComplete(object sender, EventArgs e)
+        {
+            label2.Text = "page init complete <br>";
+        }
+        protected void page_LoadComplete(object sender, EventArgs e)
+        {
+            label4.Text = "page load complete<br>";
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            if (rbtnTelugu.Checked)
+            {
+                lbltelugu.Text = "selected language is telugu";
+            }
+            else if (rbtnhindi.Checked)
+            {
+                lblhindi.Text = "selected language is hindi";
+            }
+            else if (rbtnEnglish.Checked)
+            {
+                lblenglish.Text = "selected language is english";
+            }
+        }
+
+        protected void txtusername_TextChanged(object sender, EventArgs e)
+        {
+            if (txtusername.Text.Length > 0)
+            {
+                btnsubmit.Visible = true;
+            }
+        }
+
+        protected void rbtnTelugu_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnTelugu.Checked)
+            {
+                lbltelugu.Text = "selected language is telugu";
+            }
+        }
+
+        protected void rbtnhindi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnhindi.Checked)
+            {
+                lblhindi.Text = "selected language is:Hindi";
+            }
+        }
+
+        protected void rbtnEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnEnglish.Checked)
+            {
+                lblenglish.Text = "selected language is:English";
+            }
+        }
+
+        protected void chkchess_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkchess.Checked)
+            {
+                lblmesg.Text = "chess is selected";
+            }
+        }
+
+        protected void chkTennis_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkTennis.Checked)
+            {
+                lbltennis.Text="tennis is selected";
+            }
+        }
+
+        protected void chkcricket_CheckedChanged(object sender, EventArgs e)
+        {
+            if( chkcricket.Checked)
+            {
+                lblcricket.Text = "cricket is selected";
+            }
+        }
+    }
+
+}
